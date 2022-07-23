8b11811338f41baa6359ba01e235ef2313d9804e
Aivan <aish3n@pm.me>
2022-07-23 12:49:19 +0800

    feat: decrease speed

--- src/tetris/cfg.rs
+++ src/tetris/cfg.rs
@@ -9,7 +9,7 @@ impl Default for Settings {
     fn default() -> Self {
         Self {
             window_size: (500, 700),
-            speed: Duration::from_millis(250),
+            speed: Duration::from_millis(500),
         }
     }
 }
